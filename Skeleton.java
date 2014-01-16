c7074f4cb6302f86149e3d54c53fa970013b065e
akshay288 <akshay.shekhawat@gmail.com>
2014-01-15 21:19:34 -0600

    Update Skeleton.java
    added comments

--- Skeleton.java
+++ Skeleton.java
@@ -4,19 +4,26 @@ import javax.swing.ImageIcon;
 import java.util.Random;
 
 public class Skeleton {
+	
+	//variables
 	public int xcoord;
 	public int ycoord;
 	Image Skeleton;
 	public Random generator = new Random();
 	public boolean visible = false;
 	
+	//constructor
 	public Skeleton() {
+		//loads the image
 		ImageIcon i = new ImageIcon(this.getClass().getResource("Skeleton.png"));
 		Skeleton = i.getImage();
+		
+		//sets initial x and y coordinates
 		xcoord = 500;
 		ycoord = 500;
 	}
-
+	
+	//moves the skeleten towards the knight(knights x and y coordinates are in the parameters)
 	public void move(int X, int Y) {
 		if (X > xcoord) {
 			xcoord++;
@@ -31,27 +38,35 @@ public class Skeleton {
 			ycoord--;
 		}
 	}
-
+	
+	//returns x coordinate
 	public int getX() {
 		return xcoord;
 	}
-
+	
+	//returns y coordinate
 	public int getY() {
 		return ycoord;
 	}
-
+	
+	//returns the image
 	public Image getImage() {
 		return Skeleton;
 	}
 	
+	//checks collisions
 	public boolean checkCollisions(Rectangle knightRect, boolean k){
+		//makes a rectangle around the skeleton
 		Rectangle skeletonRect = new Rectangle(this.getX(), this.getY(), 48-12, 48);
+		//if it intersects with the knight and it is attacking, the skeleton moves out of the map
 		if ((skeletonRect.intersects(knightRect)) && (k == false)) {
+			//different temporary options for every corner of the map
 			int ytemp1 = -1 * (generator.nextInt(100) + 100);
 			int xtemp1 = -1 * (generator.nextInt(100) + 100);
 			int ytemp2 = (generator.nextInt(412) + 300);
 			int xtemp2 = (generator.nextInt(412) + 300);
 			int option = generator.nextInt(4) + 1;
+			//switch chooses between the top left, top right, bottom left or bottom right corner
 			switch (option) {
 			case 1:
 				this.xcoord = xtemp1;
@@ -76,6 +91,7 @@ public class Skeleton {
 		}
 	}
 	
+	//just returns true or false based on where the knight is and if it is intersecting(smaller version of previous method)
 	public boolean checkknightCollisions(Rectangle knightRect){
 		Rectangle skeletonRect = new Rectangle(this.getX(), this.getY(), 48, 48);
 		if ((skeletonRect.intersects(knightRect))){
@@ -85,6 +101,7 @@ public class Skeleton {
 		}
 	}
 	
+	//method to make the skeleton visible
 	public void makeVisible(){
 		visible = true;
 	}
