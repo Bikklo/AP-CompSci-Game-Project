da101c954204f1fba494d97c7bf34a2ac7f48f08
akshay288 <akshay.shekhawat@gmail.com>
2014-01-16 07:26:05 -0600

    Update Knight.java
    added comments

--- Knight.java
+++ Knight.java
@@ -3,53 +3,69 @@ import java.awt.event.KeyEvent;
 import javax.swing.ImageIcon;
 
 public class Knight {
+
+	// variables
 	public boolean notAttacking = true;
-	public boolean left = false;
-	public int dx;
-	public int dy;
+	public boolean knightgoingLeft = false;
+	public int changeinX;
+	public int changeinY;
 	public int xcoord;
 	public int ycoord;
+
+	// initializes the images we will be using
 	Image knight;
 	Image knightAttack;
 	Image knightMirror;
 	Image knightAttackMirror;
 
+	// constructor
 	public Knight() {
-		ImageIcon ii = new ImageIcon(this.getClass().getResource("Knight.png"));
-		ImageIcon iii = new ImageIcon(this.getClass().getResource(
-				"Knight Attack.png"));
-		ImageIcon i = new ImageIcon(this.getClass().getResource(
-				"Knight Left.png"));
-		ImageIcon iiii = new ImageIcon(this.getClass().getResource(
-				"Knight Attack Left.png"));
-		knight = ii.getImage();
-		knightAttack = iii.getImage();
-		knightMirror = i.getImage();
-		knightAttackMirror = iiii.getImage();
+		// loads all the different images knight will need ie. mirror and mirror
+		// attack
+		ImageIcon knightTemp = new ImageIcon(this.getClass().getResource(
+				"Knight.png"));
+		ImageIcon knightAttackTemp = new ImageIcon(this.getClass().getResource(
+				"KnightAttack.png"));
+		ImageIcon knightLeftTemp = new ImageIcon(this.getClass().getResource(
+				"KnightLeft.png"));
+		ImageIcon knightAttackMirrorTemp = new ImageIcon(this.getClass()
+				.getResource("Knight Attack Left.png"));
+		knight = knightTemp.getImage();
+		knightAttack = knightAttackTemp.getImage();
+		knightMirror = knightLeftTemp.getImage();
+		knightAttackMirror = knightAttackMirrorTemp.getImage();
+
+		// sets the initial x coordinates
 		xcoord = 48;
 		ycoord = 48;
 	}
 
+	// moves the knight by changing the coordinates
 	public void move() {
-		xcoord += dx;
-		ycoord += dy;
+		xcoord += changeinX;
+		ycoord += changeinY;
 	}
 
+	// returns the xcoord for boards use
 	public int getX() {
 		return xcoord;
 	}
 
+	// returns the ycoord for boards use
 	public int getY() {
 		return ycoord;
 	}
 
+	// returns the image based on what direction the knight is going
 	public Image getImage() {
-		if (left == false) {
+		// when it is going right it returns the normal pictures
+		if (knightgoingLeft == false) {
 			if (notAttacking) {
 				return knight;
 			} else {
 				return knightAttack;
 			}
+			// mirror pictures if going left
 		} else {
 			if (notAttacking) {
 				return knightMirror;
@@ -59,58 +75,75 @@ public class Knight {
 		}
 	}
 
+	// handles a key press
 	public void keyPressed(KeyEvent e) {
+
+		// gets what key is pressed
 		int key = e.getKeyCode();
+
+		// x key if for attack
 		if ((key == KeyEvent.VK_X)) {
 			notAttacking = false;
 		}
+
+		// only goes left if it would result in the knight being inbounds
 		if ((key == KeyEvent.VK_LEFT) && (xcoord > 0)) {
-			left = true;
-			dx = -4;
+			// changes the direction the knight is going
+			knightgoingLeft = true;
+			changeinX = -4;
 		} else if (xcoord <= 0) {
-			dx = 0;
+			changeinX = 0;
 			xcoord = -1;
 		}
+
+		// only goes right if it would result in the knight being inbounds
 		if ((key == KeyEvent.VK_RIGHT) && (xcoord < 464)) {
-			dx = 4;
-			left = false;
+			changeinX = 4;
+			// changes the direction the knight is going
+			knightgoingLeft = false;
 		} else if (xcoord >= 464) {
-			dx = 0;
+			changeinX = 0;
 			xcoord = 463;
 		}
+
+		// only goes up if it would result in the knight being inbounds
 		if (key == KeyEvent.VK_UP && ycoord > 0) {
-			dy = -4;
+			changeinY = -4;
 		} else if (ycoord <= 0) {
-			dy = 0;
+			changeinY = 0;
 			ycoord = -1;
 		}
+
+		// only goes down if it would result in the knight being inbounds
 		if ((key == KeyEvent.VK_DOWN) && (ycoord < 436)) {
-			dy = 4;
+			changeinY = 4;
 		} else if (ycoord >= 436) {
-			dy = 0;
+			changeinY = 0;
 			ycoord = 435;
 		}
+
 	}
 
+	// handles a keyRelease
 	public void keyReleased(KeyEvent e) {
 		int key = e.getKeyCode();
 		if ((key == KeyEvent.VK_X)) {
 			notAttacking = true;
 		}
 		if (key == KeyEvent.VK_LEFT) {
-			dx = 0;
+			changeinX = 0;
 		}
 
 		if (key == KeyEvent.VK_RIGHT) {
-			dx = 0;
+			changeinX = 0;
 		}
 
 		if (key == KeyEvent.VK_UP) {
-			dy = 0;
+			changeinY = 0;
 		}
 
 		if (key == KeyEvent.VK_DOWN) {
-			dy = 0;
+			changeinY = 0;
 		}
+
 	}
-}
