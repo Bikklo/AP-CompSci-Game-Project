2c8cff44780bc63d4e4455e9274d8ab302bc8859
michael-walgren <michael.walgren@gmail.com>
2014-01-16 21:17:34 -0600

    Update Game.java

--- Game.java
+++ Game.java
@@ -2,17 +2,17 @@ import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
 public class Game extends JFrame {
-	// constructor
+	// constructor makes the window
 	public Game() {
 		add(new Board());
-		setTitle("Dungeon");
+		setTitle("Skeleton Horde");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(512, 512);
 		setLocationRelativeTo(null);
 		setVisible(true);
 		setResizable(false);
 	}
-
+	// creates a new game
 	public static void main(String[] args) {
 		@SuppressWarnings("unused")
 		Game g = new Game();
