c427d48051d4f01c9a09065b98580bc3e1a7696e
michael-walgren <michael.walgren@gmail.com>
2014-01-18 21:03:51 -0600

    Update Loot.java

--- Loot.java
+++ Loot.java
@@ -9,30 +9,36 @@ public class Loot {
 	Image Loot;
 	public Random generator = new Random();
 	public int value;
+
 	// constructor that creates a new loot pile
 	public Loot() {
 		ImageIcon l = new ImageIcon(this.getClass().getResource("Loot.png"));
 		Loot = l.getImage();
 		randomizePositionandValue();
 	}
+
 	// returns the x coordinate
 	public int getX() {
 		return xcoord;
 	}
+
 	// returns the y coordinate
 	public int getY() {
 		return ycoord;
 	}
+
 	// returns the loot sprite
 	public Image getImage() {
 		return Loot;
 	}
+
 	// generates the position of the loot and its value
 	public void randomizePositionandValue() {
-		xcoord = generator.nextInt(450)+1;
-		ycoord = generator.nextInt(450)+1;
-		value = generator.nextInt(10)+1;
+		xcoord = generator.nextInt(450) + 1;
+		ycoord = generator.nextInt(450) + 1;
+		value = generator.nextInt(10) + 1;
 	}
+
 	// checks for knight collisions and adds to score and moves if true
 	public boolean checkknightCollisions(Rectangle knightRect) {
 		Rectangle skeletonRect = new Rectangle(this.getX(), this.getY(), 48, 48);
