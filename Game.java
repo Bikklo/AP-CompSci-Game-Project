5ebae454ae75f3e7921a0bf25dc4fa2145bd26bf
michael-walgren <michael.walgren@gmail.com>
2014-01-20 00:48:02 -0600

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,10 +1,15 @@
+import java.awt.Image;
 import javax.swing.JFrame;
+import javax.swing.ImageIcon;
 
 @SuppressWarnings("serial")
 public class Game extends JFrame {
 	// constructor makes the window
 	public Game() {
 		add(new Board());
+		ImageIcon i = new ImageIcon(this.getClass().getResource("Icon.png"));
+		Image icon = i.getImage();
+		setIconImage(icon);
 		setTitle("Skeleton Horde");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(512, 512);
