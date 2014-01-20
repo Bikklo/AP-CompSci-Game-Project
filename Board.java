d2c3cbfe2cfc800f18015bc43ad4a090cd6ebc0a
michael-walgren <michael.walgren@gmail.com>
2014-01-20 00:16:48 -0600

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,7 +1,5 @@
 import java.awt.*;
-
 import javax.swing.*;
-
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.Color;
@@ -19,17 +17,15 @@ public class Board extends JPanel implements ActionListener {
 	int score = 0;
 	int tempX = 0;
 	int tempY = 0;
-	Image explosion;
 	Random generator = new Random(8659);
 	Image background;
 	Timer timer;
-	Font scoreFont = new Font("Arcadepix", Font.TRUETYPE_FONT,
-			Font.ROMAN_BASELINE).deriveFont(15.0f);
+	Font scoreFont = new Font("Arcadepix plus", Font.TRUETYPE_FONT, Font.ROMAN_BASELINE).deriveFont(15.0f);
 	FontMetrics scoreFontMetrics = this.getFontMetrics(this.scoreFont);
 	public Knight k = new Knight();
 	Loot[] lootArray = new Loot[5];
 	Skeleton[] skeletonArray = new Skeleton[1000];
-	Life heart = new Life();
+	Heart heart = new Heart();
 	int health;
 	int wave;
 
@@ -78,14 +74,14 @@ public class Board extends JPanel implements ActionListener {
 		if (health > 0) {
 			k.check();
 			// rectangle for knight for collision detection
-			Rectangle knightRect = new Rectangle(k.getX(), k.getY(), 48, 48);
+			Rectangle knightRect = new Rectangle(k.getX() - 10, k.getY() - 10, 28, 28);
 
 			// sets the wave
 			wave = score / 10;
 
 			// makes the appropriate number of skeletons visible
-			if ((Math.pow(wave, 2)/4) < 999) {
-				for (int x = 0; x < (Math.pow(wave, 2)/4); x++) {
+			if ((Math.pow(wave, 2) / 4) < 999) {
+				for (int x = 0; x < (Math.pow(wave, 2) / 4); x++) {
 					skeletonArray[x].makeVisible();
 				}
 			}
@@ -96,21 +92,19 @@ public class Board extends JPanel implements ActionListener {
 					score += lootArray[x].value;
 				}
 			}
-			
+
 			if (heart.checkknightCollisions(knightRect)) {
 				health += heart.value;
 			}
-			
+
 			// goes through and handles skeleton collisions(if they are visible)
 			for (int x = 0; x < skeletonArray.length; x++) {
 				if (skeletonArray[x].visible) {
-					if (skeletonArray[x].checkCollisions(knightRect,
-							k.notAttacking)) {
+					if (skeletonArray[x].checkCollisions(knightRect, k.notAttacking)) {
 						score++;
 					} else {
 						if (k.notAttacking) {
-							if ((skeletonArray[x]
-									.checkknightCollisions(knightRect))) {
+							if ((skeletonArray[x].checkknightCollisions(knightRect))) {
 								health--;
 								skeletonArray[x].xcoord -= 100;
 							}
@@ -125,18 +119,15 @@ public class Board extends JPanel implements ActionListener {
 			// draws loots
 
 			for (int x = 0; x < 3; x++) {
-				g2d.drawImage(lootArray[x].getImage(), lootArray[x].getX(),
-						lootArray[x].getY(), this);
+				g2d.drawImage(lootArray[x].getImage(), lootArray[x].getX(), lootArray[x].getY(), this);
 			}
-			
+
 			g2d.drawImage(heart.getImage(), heart.getX(), heart.getY(), this);
 
 			// draws every skeleton(if visible)
 			for (int x = 0; x < skeletonArray.length; x++) {
 				if (skeletonArray[x].visible) {
-					g2d.drawImage(skeletonArray[x].getImage(),
-							skeletonArray[x].getX(), skeletonArray[x].getY(),
-							this);
+					g2d.drawImage(skeletonArray[x].getImage(), skeletonArray[x].getX(), skeletonArray[x].getY(), this);
 					skeletonArray[x].move(k.getX(), k.getY());
 				}
 			}
@@ -144,37 +135,17 @@ public class Board extends JPanel implements ActionListener {
 			// prints the score, health, and wave
 			g2d.setColor(Color.WHITE);
 			g2d.setFont(this.scoreFont);
-			g2d.drawString(
-					String.format("  Score: %d", this.score),
-					-20,
-					this.scoreFontMetrics.getHeight()
-							- this.scoreFontMetrics.getMaxAscent()
-							+ this.scoreFontMetrics.getAscent());
-			g2d.drawString(
-					String.format("  Health: %d", this.health),
-					-20,
-					2 * (this.scoreFontMetrics.getHeight()
-							- this.scoreFontMetrics.getMaxAscent() + this.scoreFontMetrics
-							.getAscent()));
-			g2d.drawString(
-					String.format("  Wave: %d", this.wave + 1),
-					-20,
-					3 * (this.scoreFontMetrics.getHeight()
-							- this.scoreFontMetrics.getMaxAscent() + this.scoreFontMetrics
-							.getAscent()));
+			g2d.drawString(String.format("  Score: %d", this.score), 0, this.scoreFontMetrics.getHeight() - this.scoreFontMetrics.getMaxAscent() + this.scoreFontMetrics.getAscent());
+			g2d.drawString(String.format("  Health: %d", this.health), 0, 2 * (this.scoreFontMetrics.getHeight() - this.scoreFontMetrics.getMaxAscent() + this.scoreFontMetrics.getAscent()));
+			g2d.drawString(String.format("  Wave: %d", this.wave + 1), 0, 3 * (this.scoreFontMetrics.getHeight() - this.scoreFontMetrics.getMaxAscent() + this.scoreFontMetrics.getAscent()));
 		} else {
 			// prints game over if health < 0
 			Image gameOver;
-			g2d.setColor(Color.WHITE);
-			g2d.setFont(this.scoreFont);
-			g2d.drawString(
-					String.format("  Score: %d", this.score),
-					-20,
-					this.scoreFontMetrics.getHeight()
-							- this.scoreFontMetrics.getMaxAscent()
-							+ this.scoreFontMetrics.getAscent());
-			ImageIcon gO = new ImageIcon(this.getClass().getResource(
-					"Game Over.png"));
+			g2d.setColor(Color.RED);
+			Font finalScoreFont = new Font("Arcadepix plus", Font.TRUETYPE_FONT, Font.ROMAN_BASELINE).deriveFont(45.0f);
+			g2d.setFont(finalScoreFont);
+			g2d.drawString(String.format("  Score: %d", this.score), 150, 325);
+			ImageIcon gO = new ImageIcon(this.getClass().getResource("Game Over.png"));
 			gameOver = gO.getImage();
 			g2d.drawImage(gameOver, 0, 0, null);
 		}
@@ -205,8 +176,7 @@ public class Board extends JPanel implements ActionListener {
 
 	// loads the background image(dirt floor)
 	public void loadBackgroundImage() {
-		ImageIcon image = new ImageIcon(this.getClass().getResource(
-				"Background.png"));
+		ImageIcon image = new ImageIcon(this.getClass().getResource("Background.png"));
 		background = image.getImage();
 	}
 
